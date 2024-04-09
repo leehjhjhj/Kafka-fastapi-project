855304b79ef4cdfd421e9ee01383d677504e6a1e
leehjhjhj <tsukiakarii@naver.com>
2024-04-09 14:43:46 +0900

    feat: 원본 기사 컨슈머 틀 생성

---
+++ src/pipeline/consumer/raw_article_consumer.py
@@ -0,0 +1,39 @@
+from aiokafka import AIOKafkaConsumer
+from pipeline.kafka_config import KAFKA_BOOTSTRAP_SERVERS, KAFKA_TOPIC_NAME, VALUE_DESERIALIZER
+from abc import abstractmethod, ABC
+
+class Consumer(ABC):
+    @abstractmethod
+    async def start(self):
+        pass
+    
+    @abstractmethod
+    async def stop(self):
+        pass
+
+    @abstractmethod
+    async def consume(self):
+        pass
+
+class RawArticleConsumer(Consumer):
+    def __init__(self, group_id):
+        self.consumer = AIOKafkaConsumer(
+            KAFKA_TOPIC_NAME,
+            bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
+            group_id=group_id,
+            value_deserializer=VALUE_DESERIALIZER,
+            auto_offset_reset='earliest'
+        )
+
+    async def start(self):
+        await self.consumer.start()
+
+    async def stop(self):
+        await self.consumer.stop()
+
+    async def consume(self):
+        try:
+            async for msg in self.consumer:
+                print("consumed: ", msg)
+        finally:
+            await self.consumer.stop()
