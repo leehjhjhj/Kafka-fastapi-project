82777b8cd006e8531c4a96756dc1080f33197923
leehjhjhj <tsukiakarii@naver.com>
2024-04-09 14:42:55 +0900

    feat: 비동기 컨슈머 앱 실행시 시작

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from pipeline.producer.producer_router import producer_route
+from pipeline.consumer.consumer_router import consume
+import asyncio
 
 app = FastAPI()
 app.include_router(producer_route)
+asyncio.create_task(consume())
 
 origins = [
     "*",
