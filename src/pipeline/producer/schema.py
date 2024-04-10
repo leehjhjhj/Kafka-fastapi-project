36905acb7db63ff7d4921caf0440ccfccb4e0231
leehjhjhj <tsukiakarii@naver.com>
2024-04-10 09:36:28 +0900

    refactor: 기사를 받기 위한 article schema message 수정

--- src/pipeline/producer/schema.py
+++ src/pipeline/producer/schema.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Message(BaseModel):
     title: str
     link: str
-    img_link: str
+    img_link: Optional[str]
+    source_language: str
 
 class KafkaResponse(BaseModel):
     message: str
