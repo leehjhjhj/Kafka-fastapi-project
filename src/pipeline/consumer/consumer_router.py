13ff23cbc1174927c083e9c719dd907d61f7d581
leehjhjhj <tsukiakarii@naver.com>
2024-04-11 13:19:29 +0900

    refactor: gather로 동시에 시작하게 변경

--- src/pipeline/consumer/consumer_router.py
+++ src/pipeline/consumer/consumer_router.py
@@ -1,9 +1,16 @@
 from fastapi import APIRouter
-from .raw_article_consumer import RawArticleConsumer
+from .consumers.raw_article_consumer import RawArticleConsumer
+from .consumers.translating_consumer import TranslatingConsumer
+import asyncio
 
 consumer_route = APIRouter()
 
 async def consume():
     raw_article_consumer = RawArticleConsumer(group_id="test-group")
+    translating_article_consumer = TranslatingConsumer(group_id='translation-consumer-group')
     await raw_article_consumer.start()
-    await raw_article_consumer.consume()
+    await translating_article_consumer.start()
+    await asyncio.gather(
+        raw_article_consumer.consume(),
+        translating_article_consumer.consume()
+    )
