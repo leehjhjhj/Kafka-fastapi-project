170d6ab3bcb7fb6384bb52fea3a1426e38ffcdf9
leehjhjhj <tsukiakarii@naver.com>
2024-04-11 13:15:47 +0900

    add: 디테일 추가에 따른 스키마 항목 추가

--- src/pipeline/producer/schema.py
+++ src/pipeline/producer/schema.py
@@ -5,7 +5,10 @@ class Message(BaseModel):
     title: str
     link: str
     img_link: Optional[str]
+    date: str
+    content: str
     source_language: str
+    source_newspaper: str
 
 class KafkaResponse(BaseModel):
     message: str
