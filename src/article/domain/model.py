43e57b74a4808589ca8cd0426276f5dd98e251f6
leehjhjhj <tsukiakarii@naver.com>
2024-04-12 18:02:21 +0900

    refactor: date 길이 수정 및 소스 신문사 컬럼 추가

--- src/article/domain/model.py
+++ src/article/domain/model.py
@@ -1,5 +1,5 @@
 from config import Base
-from sqlalchemy import Column, BigInteger, String, Enum, Integer, DateTime
+from sqlalchemy import Column, BigInteger, String, Integer, DateTime
 
 class Article(Base):
     __tablename__ = "articles"
@@ -11,10 +11,10 @@ class Article(Base):
     content_jp = Column(String(3000), nullable=False)
     original_article_url = Column(String(255), nullable=False)
     thumbnail_url = Column(String(255), nullable=True)
-    original_language_code = Column(String(255), nullable=False)
     preview_content_kr = Column(String(100), nullable=False)
     preview_content_jp = Column(String(100), nullable=False)
-    date = Column(String(8), nullable=False)
+    date = Column(String(30), nullable=False)
     create_at = Column(DateTime, nullable=False)
     original_language_code = Column(String(8), nullable=False)
+    source_newspaper = Column(String(50), nullable=False)
     view_count = Column(Integer, default=0)
