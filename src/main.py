883225b054c5268696ec27958f91160a0fc2aade
leehjhjhj <tsukiakarii@naver.com>
2024-04-13 21:21:54 +0900

    add: article router 추가

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from pipeline.producer.producer_router import producer_route
 from pipeline.consumer.consumer_router import consume
+from article.router import article_router
 import asyncio
 
 app = FastAPI()
 app.include_router(producer_route)
+app.include_router(article_router)
 asyncio.create_task(consume())
 
 origins = [
