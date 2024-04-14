c3b75be0248a2d1b978588798697517a3bf5cc74
leehjhjhj <tsukiakarii@naver.com>
2024-04-15 00:39:01 +0900

    feat: 아티클 디테일 조회 라우터 추가

--- src/article/router.py
+++ src/article/router.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, HTTPException
-from article.infrastructure.schemas.article import ArticleListResponse
+from article.infrastructure.schemas.article import ArticleListResponse, ArticleDetailResponse
 from .container import ArticleContainer
 
 article_router = APIRouter()
 
-@article_router.get('/', response_model=list[ArticleListResponse], response_model_by_alias=True)
+@article_router.get('/', response_model=list[ArticleListResponse])
 async def list(
     language: str = 'kr'
 ):
@@ -12,5 +12,17 @@ async def list(
         article_service = ArticleContainer.article_service()
         article_list_response = await article_service.get_article_list(language)
         return article_list_response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@article_router.get('/{article_id}', response_model=ArticleDetailResponse)
+async def detail(
+    article_id: int,
+    language: str = 'kr'
+):
+    try:
+        article_service = ArticleContainer.article_service()
+        article_detail_response = await article_service.get_article_detail(article_id, language)
+        return article_detail_response
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
