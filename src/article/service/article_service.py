e9b774d152ddc0109e971f484c3fcf0ae299d9b0
leehjhjhj <tsukiakarii@naver.com>
2024-04-14 14:21:20 +0900

    refactor: uow 및 레포지토리 생성으로 인한 로직 분리

--- src/article/service/article_service.py
+++ src/article/service/article_service.py
@@ -1,15 +1,16 @@
-from config import AsyncSession
 from article.domain.model import Article
-from sqlalchemy import select
 from article.infrastructure.schemas.article import ArticleListResponse
+from common.infrastructure.uow import UnitOfWork
 
 class ArticleService:
+    def __init__(self, uof: UnitOfWork):
+        self._uof = uof
+
     async def get_article_list(self, language: str):
-        async with AsyncSession() as session:
-            results = await session.execute(select(Article))
-            articles = results.scalars().all()
-            articles = [self._convert_article(article=article, language=language) for article in articles]
-            return articles
+        async with self._uof:
+            articles = await self._uof.repo.find_all()
+            response_articles = [self._convert_article(article=article, language=language) for article in articles]
+            return response_articles
             
     def _convert_article(self, article: Article, language: str):
         title = article.title_jp if language == 'jp' else article.title_kr
