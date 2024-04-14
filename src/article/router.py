781634fb86f8a3c8860a74d3b3ed65166ea451e4
leehjhjhj <tsukiakarii@naver.com>
2024-04-14 14:22:50 +0900

    feat: get all 엔드포인트 추가

--- src/article/router.py
+++ src/article/router.py
@@ -1,16 +1,16 @@
-from fastapi import APIRouter, HTTPException, Depends
-from .service.article_service import ArticleService
-from infrastructure.schemas.article import ArticleListResponse
+from fastapi import APIRouter, HTTPException
+from article.infrastructure.schemas.article import ArticleListResponse
+from .container import ArticleContainer
 
 article_router = APIRouter()
 
 @article_router.get('/', response_model=list[ArticleListResponse], response_model_by_alias=True)
 async def list(
-    language: str = 'kr',
-    article_service: ArticleService = Depends()
+    language: str = 'kr'
 ):
-    # try:
-    article_list_response = await article_service.get_article_list(language)
-    return article_list_response
-    # except Exception as e:
-    #     raise HTTPException(status_code=500, detail=str(e))
+    try:
+        article_service = ArticleContainer.article_service()
+        article_list_response = await article_service.get_article_list(language)
+        return article_list_response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
