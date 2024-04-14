a79e5fa2f6bc570a0e4849d1650de0b34c0dc07a
leehjhjhj <tsukiakarii@naver.com>
2024-04-14 21:04:00 +0900

    refactor: KAFKA_BOOTSTRAP_SERVERS를 env로 관리하도록 변경

--- src/pipeline/kafka_config.py
+++ src/pipeline/kafka_config.py
@@ -1,8 +1,12 @@
 import json
 import asyncio
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 loop = asyncio.get_event_loop()
-KAFKA_BOOTSTRAP_SERVERS = ['my-kafka:9092']
+KAFKA_BOOTSTRAP_SERVERS = os.environ['KAFKA_BOOTSTRAP_SERVERS']
 KAFKA_TOPIC_NAME = 'test'
 VALUE_SERIALIZER = str.encode
 VALUE_DESERIALIZER = lambda x: json.loads(x.decode('utf-8'))
